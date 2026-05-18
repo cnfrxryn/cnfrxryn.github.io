f2afe143074f5a511434efa97a676de265ea2b60
Francine Villareal <cinefra.tinythinkers@gmail.com>
2026-05-19 02:14:17 +0800

    Added api for live exchange

--- global/js/currency-data.js
+++ global/js/currency-data.js
@@ -286,5 +286,689 @@ const currencies = [
     "name": "Faroese króna",
     "symbol": "kr",
     "countryCode": "fo"
+  },
+  {
+    "code": "GBP",
+    "name": "British pound",
+    "symbol": "£",
+    "countryCode": "gb"
+  },
+  {
+    "code": "GEL",
+    "name": "lari",
+    "symbol": "₾",
+    "countryCode": "ge"
+  },
+  {
+    "code": "GGP",
+    "name": "Guernsey pound",
+    "symbol": "£",
+    "countryCode": "gg"
+  },
+  {
+    "code": "GHS",
+    "name": "Ghanaian cedi",
+    "symbol": "₵",
+    "countryCode": "gh"
+  },
+  {
+    "code": "GIP",
+    "name": "Gibraltar pound",
+    "symbol": "£",
+    "countryCode": "gi"
+  },
+  {
+    "code": "GMD",
+    "name": "dalasi",
+    "symbol": "D",
+    "countryCode": "gm"
+  },
+  {
+    "code": "GNF",
+    "name": "Guinean franc",
+    "symbol": "Fr",
+    "countryCode": "gn"
+  },
+  {
+    "code": "GTQ",
+    "name": "Guatemalan quetzal",
+    "symbol": "Q",
+    "countryCode": "gt"
+  },
+  {
+    "code": "GYD",
+    "name": "Guyanese dollar",
+    "symbol": "$",
+    "countryCode": "gy"
+  },
+  {
+    "code": "HKD",
+    "name": "Hong Kong dollar",
+    "symbol": "$",
+    "countryCode": "hk"
+  },
+  {
+    "code": "HNL",
+    "name": "Honduran lempira",
+    "symbol": "L",
+    "countryCode": "hn"
+  },
+  {
+    "code": "HTG",
+    "name": "Haitian gourde",
+    "symbol": "G",
+    "countryCode": "ht"
+  },
+  {
+    "code": "HUF",
+    "name": "Hungarian forint",
+    "symbol": "Ft",
+    "countryCode": "hu"
+  },
+  {
+    "code": "IDR",
+    "name": "Indonesian rupiah",
+    "symbol": "Rp",
+    "countryCode": "id"
+  },
+  {
+    "code": "ILS",
+    "name": "Israeli new shekel",
+    "symbol": "₪",
+    "countryCode": "ps"
+  },
+  {
+    "code": "IMP",
+    "name": "Manx pound",
+    "symbol": "£",
+    "countryCode": "im"
+  },
+  {
+    "code": "INR",
+    "name": "Indian rupee",
+    "symbol": "₹",
+    "countryCode": "bt"
+  },
+  {
+    "code": "IQD",
+    "name": "Iraqi dinar",
+    "symbol": "ع.د",
+    "countryCode": "iq"
+  },
+  {
+    "code": "IRR",
+    "name": "Iranian rial",
+    "symbol": "﷼",
+    "countryCode": "ir"
+  },
+  {
+    "code": "ISK",
+    "name": "Icelandic króna",
+    "symbol": "kr",
+    "countryCode": "is"
+  },
+  {
+    "code": "JEP",
+    "name": "Jersey pound",
+    "symbol": "£",
+    "countryCode": "je"
+  },
+  {
+    "code": "JMD",
+    "name": "Jamaican dollar",
+    "symbol": "$",
+    "countryCode": "jm"
+  },
+  {
+    "code": "JOD",
+    "name": "Jordanian dinar",
+    "symbol": "JD",
+    "countryCode": "ps"
+  },
+  {
+    "code": "JPY",
+    "name": "Japanese yen",
+    "symbol": "¥",
+    "countryCode": "jp"
+  },
+  {
+    "code": "KES",
+    "name": "Kenyan shilling",
+    "symbol": "Sh",
+    "countryCode": "ke"
+  },
+  {
+    "code": "KGS",
+    "name": "Kyrgyzstani som",
+    "symbol": "с",
+    "countryCode": "kg"
+  },
+  {
+    "code": "KHR",
+    "name": "Cambodian riel",
+    "symbol": "៛",
+    "countryCode": "kh"
+  },
+  {
+    "code": "KID",
+    "name": "Kiribati dollar",
+    "symbol": "$",
+    "countryCode": "ki"
+  },
+  {
+    "code": "KMF",
+    "name": "Comorian franc",
+    "symbol": "Fr",
+    "countryCode": "km"
+  },
+  {
+    "code": "KPW",
+    "name": "North Korean won",
+    "symbol": "₩",
+    "countryCode": "kp"
+  },
+  {
+    "code": "KRW",
+    "name": "South Korean won",
+    "symbol": "₩",
+    "countryCode": "kr"
+  },
+  {
+    "code": "KWD",
+    "name": "Kuwaiti dinar",
+    "symbol": "د.ك",
+    "countryCode": "kw"
+  },
+  {
+    "code": "KYD",
+    "name": "Cayman Islands dollar",
+    "symbol": "$",
+    "countryCode": "ky"
+  },
+  {
+    "code": "KZT",
+    "name": "Kazakhstani tenge",
+    "symbol": "₸",
+    "countryCode": "kz"
+  },
+  {
+    "code": "LAK",
+    "name": "Lao kip",
+    "symbol": "₭",
+    "countryCode": "la"
+  },
+  {
+    "code": "LBP",
+    "name": "Lebanese pound",
+    "symbol": "ل.ل",
+    "countryCode": "lb"
+  },
+  {
+    "code": "LKR",
+    "name": "Sri Lankan rupee",
+    "symbol": "Rs  රු",
+    "countryCode": "lk"
+  },
+  {
+    "code": "LRD",
+    "name": "Liberian dollar",
+    "symbol": "$",
+    "countryCode": "lr"
+  },
+  {
+    "code": "LSL",
+    "name": "Lesotho loti",
+    "symbol": "L",
+    "countryCode": "ls"
+  },
+  {
+    "code": "LYD",
+    "name": "Libyan dinar",
+    "symbol": "ل.د",
+    "countryCode": "ly"
+  },
+  {
+    "code": "MAD",
+    "name": "Moroccan dirham",
+    "symbol": "د.م.",
+    "countryCode": "ma"
+  },
+  {
+    "code": "MDL",
+    "name": "Moldovan leu",
+    "symbol": "L",
+    "countryCode": "md"
+  },
+  {
+    "code": "MGA",
+    "name": "Malagasy ariary",
+    "symbol": "Ar",
+    "countryCode": "mg"
+  },
+  {
+    "code": "MKD",
+    "name": "denar",
+    "symbol": "den",
+    "countryCode": "mk"
+  },
+  {
+    "code": "MMK",
+    "name": "Burmese kyat",
+    "symbol": "Ks",
+    "countryCode": "mm"
+  },
+  {
+    "code": "MNT",
+    "name": "Mongolian tögrög",
+    "symbol": "₮",
+    "countryCode": "mn"
+  },
+  {
+    "code": "MOP",
+    "name": "Macanese pataca",
+    "symbol": "P",
+    "countryCode": "mo"
+  },
+  {
+    "code": "MRU",
+    "name": "Mauritanian ouguiya",
+    "symbol": "UM",
+    "countryCode": "mr"
+  },
+  {
+    "code": "MUR",
+    "name": "Mauritian rupee",
+    "symbol": "₨",
+    "countryCode": "mu"
+  },
+  {
+    "code": "MVR",
+    "name": "Maldivian rufiyaa",
+    "symbol": ".ރ",
+    "countryCode": "mv"
+  },
+  {
+    "code": "MWK",
+    "name": "Malawian kwacha",
+    "symbol": "MK",
+    "countryCode": "mw"
+  },
+  {
+    "code": "MXN",
+    "name": "Mexican peso",
+    "symbol": "$",
+    "countryCode": "mx"
+  },
+  {
+    "code": "MYR",
+    "name": "Malaysian ringgit",
+    "symbol": "RM",
+    "countryCode": "my"
+  },
+  {
+    "code": "MZN",
+    "name": "Mozambican metical",
+    "symbol": "MT",
+    "countryCode": "mz"
+  },
+  {
+    "code": "NAD",
+    "name": "Namibian dollar",
+    "symbol": "$",
+    "countryCode": "na"
+  },
+  {
+    "code": "NGN",
+    "name": "Nigerian naira",
+    "symbol": "₦",
+    "countryCode": "ng"
+  },
+  {
+    "code": "NIO",
+    "name": "Nicaraguan córdoba",
+    "symbol": "C$",
+    "countryCode": "ni"
+  },
+  {
+    "code": "NOK",
+    "name": "krone",
+    "symbol": "kr",
+    "countryCode": "sj"
+  },
+  {
+    "code": "NPR",
+    "name": "Nepalese rupee",
+    "symbol": "₨",
+    "countryCode": "np"
+  },
+  {
+    "code": "NZD",
+    "name": "New Zealand dollar",
+    "symbol": "$",
+    "countryCode": "pn"
+  },
+  {
+    "code": "OMR",
+    "name": "Omani rial",
+    "symbol": "ر.ع.",
+    "countryCode": "om"
+  },
+  {
+    "code": "PAB",
+    "name": "Panamanian balboa",
+    "symbol": "B/.",
+    "countryCode": "pa"
+  },
+  {
+    "code": "PEN",
+    "name": "Peruvian sol",
+    "symbol": "S/ ",
+    "countryCode": "pe"
+  },
+  {
+    "code": "PGK",
+    "name": "Papua New Guinean kina",
+    "symbol": "K",
+    "countryCode": "pg"
+  },
+  {
+    "code": "PHP",
+    "name": "Philippine peso",
+    "symbol": "₱",
+    "countryCode": "ph"
+  },
+  {
+    "code": "PKR",
+    "name": "Pakistani rupee",
+    "symbol": "₨",
+    "countryCode": "pk"
+  },
+  {
+    "code": "PLN",
+    "name": "Polish złoty",
+    "symbol": "zł",
+    "countryCode": "pl"
+  },
+  {
+    "code": "PYG",
+    "name": "Paraguayan guaraní",
+    "symbol": "₲",
+    "countryCode": "py"
+  },
+  {
+    "code": "QAR",
+    "name": "Qatari riyal",
+    "symbol": "ر.ق",
+    "countryCode": "qa"
+  },
+  {
+    "code": "RON",
+    "name": "Romanian leu",
+    "symbol": "lei",
+    "countryCode": "ro"
+  },
+  {
+    "code": "RSD",
+    "name": "Serbian dinar",
+    "symbol": "дин.",
+    "countryCode": "rs"
+  },
+  {
+    "code": "RUB",
+    "name": "Russian ruble",
+    "symbol": "₽",
+    "countryCode": "ru"
+  },
+  {
+    "code": "RWF",
+    "name": "Rwandan franc",
+    "symbol": "Fr",
+    "countryCode": "rw"
+  },
+  {
+    "code": "SAR",
+    "name": "Saudi riyal",
+    "symbol": "ر.س",
+    "countryCode": "sa"
+  },
+  {
+    "code": "SBD",
+    "name": "Solomon Islands dollar",
+    "symbol": "$",
+    "countryCode": "sb"
+  },
+  {
+    "code": "SCR",
+    "name": "Seychellois rupee",
+    "symbol": "₨",
+    "countryCode": "sc"
+  },
+  {
+    "code": "SDG",
+    "name": "Sudanese pound",
+    "symbol": "ج.س",
+    "countryCode": "sd"
+  },
+  {
+    "code": "SEK",
+    "name": "Swedish krona",
+    "symbol": "kr",
+    "countryCode": "se"
+  },
+  {
+    "code": "SGD",
+    "name": "Singapore dollar",
+    "symbol": "$",
+    "countryCode": "bn"
+  },
+  {
+    "code": "SHP",
+    "name": "Saint Helena pound",
+    "symbol": "£",
+    "countryCode": "sh"
+  },
+  {
+    "code": "SLE",
+    "name": "Leone",
+    "symbol": "Le",
+    "countryCode": "sl"
+  },
+  {
+    "code": "SOS",
+    "name": "Somali shilling",
+    "symbol": "Sh",
+    "countryCode": "so"
+  },
+  {
+    "code": "SRD",
+    "name": "Surinamese dollar",
+    "symbol": "$",
+    "countryCode": "sr"
+  },
+  {
+    "code": "SSP",
+    "name": "South Sudanese pound",
+    "symbol": "£",
+    "countryCode": "ss"
+  },
+  {
+    "code": "STN",
+    "name": "São Tomé and Príncipe dobra",
+    "symbol": "Db",
+    "countryCode": "st"
+  },
+  {
+    "code": "SYP",
+    "name": "Syrian pound",
+    "symbol": "£",
+    "countryCode": "sy"
+  },
+  {
+    "code": "SZL",
+    "name": "Swazi lilangeni",
+    "symbol": "L",
+    "countryCode": "sz"
+  },
+  {
+    "code": "THB",
+    "name": "Thai baht",
+    "symbol": "฿",
+    "countryCode": "th"
+  },
+  {
+    "code": "TJS",
+    "name": "Tajikistani somoni",
+    "symbol": "ЅМ",
+    "countryCode": "tj"
+  },
+  {
+    "code": "TMT",
+    "name": "Turkmenistan manat",
+    "symbol": "m",
+    "countryCode": "tm"
+  },
+  {
+    "code": "TND",
+    "name": "Tunisian dinar",
+    "symbol": "د.ت",
+    "countryCode": "tn"
+  },
+  {
+    "code": "TOP",
+    "name": "Tongan paʻanga",
+    "symbol": "T$",
+    "countryCode": "to"
+  },
+  {
+    "code": "TRY",
+    "name": "Turkish lira",
+    "symbol": "₺",
+    "countryCode": "tr"
+  },
+  {
+    "code": "TTD",
+    "name": "Trinidad and Tobago dollar",
+    "symbol": "$",
+    "countryCode": "tt"
+  },
+  {
+    "code": "TVD",
+    "name": "Tuvaluan dollar",
+    "symbol": "$",
+    "countryCode": "tv"
+  },
+  {
+    "code": "TWD",
+    "name": "New Taiwan dollar",
+    "symbol": "$",
+    "countryCode": "tw"
+  },
+  {
+    "code": "TZS",
+    "name": "Tanzanian shilling",
+    "symbol": "Sh",
+    "countryCode": "tz"
+  },
+  {
+    "code": "UAH",
+    "name": "Ukrainian hryvnia",
+    "symbol": "₴",
+    "countryCode": "ua"
+  },
+  {
+    "code": "UGX",
+    "name": "Ugandan shilling",
+    "symbol": "Sh",
+    "countryCode": "ug"
+  },
+  {
+    "code": "USD",
+    "name": "United States dollar",
+    "symbol": "$",
+    "countryCode": "gu"
+  },
+  {
+    "code": "UYU",
+    "name": "Uruguayan peso",
+    "symbol": "$",
+    "countryCode": "uy"
+  },
+  {
+    "code": "UZS",
+    "name": "Uzbekistani soʻm",
+    "symbol": "so'm",
+    "countryCode": "uz"
+  },
+  {
+    "code": "VES",
+    "name": "Venezuelan bolívar soberano",
+    "symbol": "Bs.S.",
+    "countryCode": "ve"
+  },
+  {
+    "code": "VND",
+    "name": "Vietnamese đồng",
+    "symbol": "₫",
+    "countryCode": "vn"
+  },
+  {
+    "code": "VUV",
+    "name": "Vanuatu vatu",
+    "symbol": "Vt",
+    "countryCode": "vu"
+  },
+  {
+    "code": "WST",
+    "name": "Samoan tālā",
+    "symbol": "T",
+    "countryCode": "ws"
+  },
+  {
+    "code": "XAF",
+    "name": "Central African CFA franc",
+    "symbol": "Fr",
+    "countryCode": "td"
+  },
+  {
+    "code": "XCD",
+    "name": "Eastern Caribbean dollar",
+    "symbol": "$",
+    "countryCode": "ai"
+  },
+  {
+    "code": "XOF",
+    "name": "West African CFA franc",
+    "symbol": "Fr",
+    "countryCode": "bj"
+  },
+  {
+    "code": "XPF",
+    "name": "CFP franc",
+    "symbol": "₣",
+    "countryCode": "pf"
+  },
+  {
+    "code": "YER",
+    "name": "Yemeni rial",
+    "symbol": "﷼",
+    "countryCode": "ye"
+  },
+  {
+    "code": "ZAR",
+    "name": "South African rand",
+    "symbol": "R",
+    "countryCode": "na"
+  },
+  {
+    "code": "ZMW",
+    "name": "Zambian kwacha",
+    "symbol": "ZK",
+    "countryCode": "zm"
+  },
+  {
+    "code": "ZWL",
+    "name": "Zimbabwean dollar",
+    "symbol": "$",
+    "countryCode": "zw"
   }
 ];
--- my-projects/currency-converter/currency-converter.js
+++ my-projects/currency-converter/currency-converter.js
@@ -7,6 +7,14 @@ const fromCurrencyCode = document.getElementById("fromCurrencyCode");
 const fromCurrencyName = document.getElementById("fromCurrencyName");
 const toCurrencyCode = document.getElementById("toCurrencyCode");
 const toCurrencyName = document.getElementById("toCurrencyName");
+const fromAmountInput = document.getElementById("fromAmountInput");
+const toAmountInput = document.getElementById("toAmountInput");
+const swapBtn = document.querySelector(".swap-btn");
+
+/* STATE */
+let fromCurrency = "USD";
+let toCurrency = "EUR";
+let exchangeRate = 0;
 
 /* RENDER DROPDOWNS */
 function renderCurrencyDropdown(dropdownElement, type, searchValue = "") {
@@ -32,20 +40,50 @@ function renderCurrencyDropdown(dropdownElement, type, searchValue = "") {
 
     option.addEventListener("click", () => {
       if (type === "from") {
+        fromCurrency = currency.code;
         fromCurrencyCode.textContent = currency.code;
         fromCurrencyName.textContent = currency.name;
         fromCurrencyDropdown.classList.remove("active");
       }
 
       else {
+        toCurrency = currency.code;
         toCurrencyCode.textContent = currency.code;
         toCurrencyName.textContent = currency.name;
         toCurrencyDropdown.classList.remove("active");
       }
+
+      fetchExchangeRate();
     });
 
     optionsList.appendChild(option);
   });
+}
+
+/* FETCH EXCHANGE RATE */
+async function fetchExchangeRate() {
+  try {
+    const response = await fetch(`https://open.er-api.com/v6/latest/${fromCurrency}`);
+    const data = await response.json();
+    exchangeRate = data.rates[toCurrency];
+    convertCurrency();
+  }
+
+  catch (error) {
+    console.error("Exchange rate fetch failed:", error);
+  }
+}
+
+/* CONVERT CURRENCY */
+function convertCurrency() {
+  const amount = parseFloat(fromAmountInput.value);
+  if (!amount) {
+    toAmountInput.value = "";
+    return;
+  }
+
+  const convertedAmount = amount * exchangeRate;
+  toAmountInput.value = convertedAmount.toFixed(2);
 }
 
 /* TOGGLE DROPDOWNS */
@@ -79,58 +117,79 @@ document.getElementById("toCurrencySearch").addEventListener("input", (e) => {
     renderCurrencyDropdown(toCurrencyDropdown, "to", e.target.value);
 });
 
+/* LIVE CONVERSION */
+fromAmountInput.addEventListener("input", convertCurrency);
+
+/* SWAP CURRENCIES */
+swapBtn.addEventListener("click", () => {
+  const tempCurrency = fromCurrency;
+  fromCurrency = toCurrency;
+  toCurrency = tempCurrency;
+
+  const tempCode = fromCurrencyCode.textContent;
+  fromCurrencyCode.textContent = toCurrencyCode.textContent;
+  toCurrencyCode.textContent = tempCode;
+
+  const tempName = fromCurrencyName.textContent;
+  fromCurrencyName.textContent = toCurrencyName.textContent;
+  toCurrencyName.textContent = tempName;
+
+  fetchExchangeRate();
+});
+
 /* INIT */
 renderCurrencyDropdown(fromCurrencyDropdown, "from");
 renderCurrencyDropdown(toCurrencyDropdown, "to");
+fetchExchangeRate();
 
-/* TO DELETE */
-const generateCurrencyDataBtn = document.getElementById("generateCurrencyDataBtn");
-const currencyDataOutput = document.getElementById("currencyDataOutput");
-generateCurrencyDataBtn.addEventListener("click", async () => {
-    try {
-      generateCurrencyDataBtn.textContent = "Generating...";
+// /* DATASET GENERATOR (Temporary only; to be re-used) */
+// const generateCurrencyDataBtn = document.getElementById("generateCurrencyDataBtn");
+// const currencyDataOutput = document.getElementById("currencyDataOutput");
+// generateCurrencyDataBtn.addEventListener("click", async () => {
+//     try {
+//       generateCurrencyDataBtn.textContent = "Generating...";
 
-      /* FETCH COUNTRIES */
-      const response = await fetch("https://restcountries.com/v3.1/all?fields=currencies,cca2");
-      const countries = await response.json();
+//       /* FETCH COUNTRIES */
+//       const response = await fetch("https://restcountries.com/v3.1/all?fields=currencies,cca2");
+//       const countries = await response.json();
 
-      /* STORE UNIQUE CURRENCIES */
-      const currencyMap = new Map();
+//       /* STORE UNIQUE CURRENCIES */
+//       const currencyMap = new Map();
 
-      countries.forEach((country) => {
-        if (!country.currencies) return;
-        Object.entries(country.currencies).forEach(([code, currencyData]) => {
-            if (!currencyMap.has(code)) {
-              currencyMap.set(code, {
-                code,
-                name: currencyData.name || "",
-                symbol: currencyData.symbol || "",
-                countryCode: (country.cca2 || "").toLowerCase()
-              });
-            }
-          });
-      });
+//       countries.forEach((country) => {
+//         if (!country.currencies) return;
+//         Object.entries(country.currencies).forEach(([code, currencyData]) => {
+//             if (!currencyMap.has(code)) {
+//               currencyMap.set(code, {
+//                 code,
+//                 name: currencyData.name || "",
+//                 symbol: currencyData.symbol || "",
+//                 countryCode: (country.cca2 || "").toLowerCase()
+//               });
+//             }
+//           });
+//       });
 
-      /* CONVERT TO ARRAY */
-      const currencyArray = Array.from(currencyMap.values());
+//       /* CONVERT TO ARRAY */
+//       const currencyArray = Array.from(currencyMap.values());
 
-      /* SORT ALPHABETICALLY */
-      currencyArray.sort((a, b) => a.code.localeCompare(b.code));
+//       /* SORT ALPHABETICALLY */
+//       currencyArray.sort((a, b) => a.code.localeCompare(b.code));
 
-      /* GENERATE JS CONTENT */
-      const finalOutput = `const currencies = ${JSON.stringify(currencyArray, null, 2)};`;
+//       /* GENERATE JS CONTENT */
+//       const finalOutput = `const currencies = ${JSON.stringify(currencyArray, null, 2)};`;
 
-      /* SHOW OUTPUT */
-      currencyDataOutput.value = finalOutput;
-      console.log(currencyArray);
+//       /* SHOW OUTPUT */
+//       currencyDataOutput.value = finalOutput;
+//       console.log(currencyArray);
 
-      generateCurrencyDataBtn.textContent = "Dataset Generated";
-    }
+//       generateCurrencyDataBtn.textContent = "Dataset Generated";
+//     }
 
-    catch (error) {
-      console.error(error);
-      currencyDataOutput.value = "Error generating currency data.";
-      generateCurrencyDataBtn.textContent = "Generation Failed";
-    }
-  }
-);
+//     catch (error) {
+//       console.error(error);
+//       currencyDataOutput.value = "Error generating currency data.";
+//       generateCurrencyDataBtn.textContent = "Generation Failed";
+//     }
+//   }
+// );
